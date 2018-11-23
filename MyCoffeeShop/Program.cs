f1e48a3db429fb22485b2a5d962ffc21a00215fc
michel ngako <emoumichel@gmail.com>
2018-11-23 16:53:01 -0500

    Add of Do while loop

--- MyCoffeeShop/Program.cs
+++ MyCoffeeShop/Program.cs
@@ -11,38 +11,54 @@ namespace MyCoffeeShop
         static void Main(string[] args)
         {
             int TotalCoffeeCost = 0;
-
-            Console.WriteLine("Please select your coffee size:\n 1- small, 2- Meduim, 3- Large");
-            int UserChoice = int.Parse(Console.ReadLine());
-
-            switch (UserChoice)
+            //string UserDecision = " "; or you can use the following
+            string UserDecision = string.Empty;
+            do
             {
-                case 1:
-                    TotalCoffeeCost += 1;
-                    break;
-                case 2:
-                    TotalCoffeeCost += 2;
-                    break;
-                case 3:
-                    TotalCoffeeCost += 3;
-                    break;
+                Console.WriteLine("Please select your coffee size:\n 1- small, 2- Meduim, 3- Large");
+                int UserChoice = int.Parse(Console.ReadLine());
 
-                default:
-                    Console.WriteLine("Your choice {0} is invalid", UserChoice);
-                    break;
+                switch (UserChoice)
+                {
+                    case 1:
+                        TotalCoffeeCost += 1;
+                        break;
+                    case 2:
+                        TotalCoffeeCost += 2;
+                        break;
+                    case 3:
+                        TotalCoffeeCost += 3;
+                        break;
+
+                    default:
+                        Console.WriteLine("Your choice {0} is invalid", UserChoice);
+                        break;
+                }
+                
+             
+
+                do
+                {
+
+                    Console.WriteLine("Do you want to buy another coffee : Yes or No?");
+
+                    UserDecision = Console.ReadLine().ToUpper();
+                    if (UserDecision != "YES" && UserDecision != "NO")
+                    {
+                        Console.WriteLine("Invalid Choice, please say Yes or NO");
+                    }
+
+                } while (UserDecision != "YES" && UserDecision != "NO");
+
             }
-
-            Console.WriteLine("Do you want to buy another coffee : Yes or No?");
-                String UserDecision = Console.ReadLine();
-
-            switch (UserDecision.ToUpper())
-            {
-
-            }
+            while (UserDecision.ToUpper() != "NO");
 
             Console.WriteLine("Thank you for shopping with us");
             Console.WriteLine("Bill Amount = {0}", TotalCoffeeCost);
+            Console.ReadLine();
 
         }
+            
+            
     }
 }
